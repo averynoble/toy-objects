bca923b62738701395ca30bea4610eebd9ea22ea
averynoble <averynoble.an@gmail.com>
2021-05-27 13:10:57 -0500

    Array of toy objects practice

---
+++ leonids-toys.js
@@ -0,0 +1,32 @@
+const toys = [
+    {
+        id: 1,
+        name: "teddy",
+        price: 5,
+        weight:2
+    },
+
+    {
+        id: 2,
+        name: "Thomas",
+        price: 8,
+        weight: 4
+    },
+
+    {
+        id: 3,
+        name: "BattleShip",
+        price: 12,
+        weight: 3
+    },
+
+    {
+        id: 4,
+        name: "Barbie",
+        price: 15,
+        weight: 2
+    }
+]
+
+console.log(toys);
+
