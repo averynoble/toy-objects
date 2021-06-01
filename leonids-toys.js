ab529697cf60a7c3e922abd58c8d2105edbb80fd
averynoble <averynoble.an@gmail.com>
2021-06-01 11:48:37 -0500

    added id function for every new toy

--- leonids-toys.js
+++ leonids-toys.js
@@ -45,8 +45,26 @@ const elmo = {
 toys.push(lego);
 toys.push(elmo);
 
-for (const toy of toys) {
-    toy.price = toy.price * 1.05
-    console.log(`The ${toy.name} costs ${toy.price} dollars and weights ${toy.weight} pounds.`);
+
+const addToyToInventory = (toyObject) => {
+    const lastIndex = toys.length - 1  // Gets index of last item in array
+    const currentLastToy = toys[lastIndex]  // Gets the last object in the array
+    const maxId = currentLastToy.id  // Get id property value of last toy
+    const idForNewToy = maxId + 1 // Increases the current max ID by 1
+
+    toyObject.id = idForNewToy // Adds the ID property to the toy object 
+    toys.push(toyObject) // Adds the toy object to the array
 }
 
+const giJoe = {
+    name: "GI Joe",
+    price: 12,
+    weight: 2
+}
+
+addToyToInventory(giJoe)
+
+for (const toy of toys) {
+    toy.price = toy.price * 1.05
+    console.log(`The ${toy.name} costs ${toy.price} dollars and weights ${toy.weight} pounds. Its ID is : ${toy.id}`);
+}
